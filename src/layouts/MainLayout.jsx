102a4aea074c581f4bb308874c4e24e82a2901b9
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2023-11-08 12:48:50 +0600

    add loading spinner

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -2,17 +2,25 @@ import { Outlet } from "react-router-dom";
 import Navbar from "../components/header/Navbar";
 import Banner from "../components/header/Banner";
 import Footer from "../components/Footer";
+import { useLocation, useNavigation } from "react-router-dom";
+import spinner from "../assets/img/Animation - 1699411161636.gif";
 
 const MainLayout = () => {
+  const location = useLocation();
+  const loading = useNavigation();
   return (
     <div className="w-full min-h-screen overflow-x-hidden font-poppins">
       <div className="container w-full md:w-90 mx-auto m-1">
         <header>
           <Navbar />
-          <Banner />
+          {location.pathname === "/" ? <Banner /> : ""}
         </header>
         <main>
-          <Outlet />
+          {loading.state === "loading" ? (
+            <img className="w-20" src={spinner} alt="loading spinner" />
+          ) : (
+            <Outlet />
+          )}
         </main>
       </div>
       <Footer />
