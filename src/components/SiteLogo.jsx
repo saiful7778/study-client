a90295bb6333d4f85fe4d6c0289c24f93c9e2ac4
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2023-11-08 12:39:18 +0600

    update site logo

--- src/components/SiteLogo.jsx
+++ src/components/SiteLogo.jsx
@@ -3,9 +3,9 @@ import siteLogo from "../assets/img/study-site-logo.png";
 
 const SiteLogo = () => {
   return (
-    <Link className="text-3xl font-bold inline-flex items-center gap-1" to="/">
+    <Link className="sitelogo" to="/">
       <img className="w-9 h-9" src={siteLogo} alt="site logo" />
-      <div className="max-sm:hidden max-min-sm:block">
+      <div className="logoText">
         <span className="text-blue-700">S</span>tudy
       </div>
     </Link>
