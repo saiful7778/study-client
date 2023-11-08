f0d4741d6f52421249d07ea3f63e40ca5e3eee67
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2023-11-08 20:47:10 +0600

    add create functions

--- src/pages/assignment/Assignments.jsx
+++ src/pages/assignment/Assignments.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 const Assignments = () => {
   return <div>Assignments</div>;
 };
--- src/pages/assignment/Create.jsx
+++ src/pages/assignment/Create.jsx
@@ -1,37 +1,92 @@
+import { useState } from "react";
+import axiosConfig from "../../config/axios.config";
+import useAuth from "../../hooks/useAuth";
+import swal from "sweetalert";
+
 const Create = () => {
+  const { userData } = useAuth();
+  const [spinner, setSpinner] = useState(false);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setSpinner(true);
+
+    const form = e.target;
+    const title = form.title.value;
+    const mark = form.mark.value;
+    const thumbnailUrl = form.thumbnailUrl.value;
+    const level = form.level.value;
+    const des = form.des.value;
+    const data = { title, mark, thumbnailUrl, level, des };
+    axiosConfig
+      .post(`/assignment/new?email=${userData?.email}`, data)
+      .then((res) => {
+        if (res.data.success) {
+          form.reset();
+          swal({
+            title: "Assignment created",
+            icon: "success",
+          });
+          setSpinner(false);
+        }
+      })
+      .catch((err) => {
+        swal({
+          title: err,
+          icon: "error",
+        });
+        console.error(err);
+        setSpinner(false);
+      });
+  };
   return (
     <div className="md:w-4/5 w-full mx-auto bg-white p-4 rounded-md shadow-md border border-gray-300 my-20">
       <h4 className="text-4xl font-bold text-primary text-center mb-4">
         Create new assignment
       </h4>
-      <form>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-3">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-3">
           <input
             type="text"
             placeholder="Title"
+            name="title"
             className="input input-bordered input-primary w-full"
             required
           />
           <input
             type="number"
             placeholder="Total marks"
+            name="mark"
             className="input input-bordered input-primary w-full"
             required
           />
           <input
-            type="file"
-            className="file-input file-input-bordered file-input-primary w-full"
-            accept="image/*"
+            type="url"
+            placeholder="Thumbnail url"
+            name="thumbnailUrl"
+            className="input input-bordered input-primary w-full"
+            required
           />
-          <select className="select select-primary w-full">
-            <option disabled selected>
-              Difficulty level
-            </option>
+          <select
+            className="select select-primary w-full"
+            defaultValue="medium"
+            name="level"
+            required
+          >
             <option value="easy">Easy</option>
             <option value="medium">Medium</option>
             <option value="hard">Hard</option>
           </select>
         </div>
+        <textarea
+          className="textarea textarea-primary"
+          placeholder="Description"
+          name="des"
+          required
+        ></textarea>
+        <button className="btn btn-primary" type="submit">
+          {spinner ? <span className="spinner"></span> : "create"}
+        </button>
       </form>
     </div>
   );
